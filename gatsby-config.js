c4304bf0e3bf684d9086f3685e18b3f1fccb9638
Glen Winters <glenwinters@gmail.com>
2020-05-25 12:42:02 -0500

    feat: add plugin to deploy to s3

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,30 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const targetAddress = new URL(
+  process.env.TARGET_ADDRESS || `https://glenwinters.local`
+);
+
 module.exports = {
-  plugins: ['gatsby-plugin-emotion', {
-    resolve: 'gatsby-plugin-google-fonts', 
-    options: {
-      fonts: ['Amatic SC\:700'],
-      display: 'swap'
-    }
-  }],
-}
+  plugins: [
+    'gatsby-plugin-emotion',
+    {
+      resolve: 'gatsby-plugin-google-fonts',
+      options: {
+        fonts: ['Amatic SC:700'],
+        display: 'swap',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: process.env.TARGET_BUCKET_NAME || 'fake-bucket',
+        region: process.env.AWS_REGION,
+        protocol: targetAddress.protocol.slice(0, -1),
+        hostname: targetAddress.hostname,
+        acl: null,
+        params: {},
+      },
+    },
+  ],
+};

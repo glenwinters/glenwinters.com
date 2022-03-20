60110b3144a6318b6eb99148e430c097454515cd
Glen Winters <glenwinters@gmail.com>
2022-03-20 05:35:29 +0000

    feat: upgrade gatsby to v3
    https://www.gatsbyjs.com/docs/reference/release-notes/migrating-from-v2-to-v3/

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { css, Global } from '@emotion/core';
+import { css, Global } from '@emotion/react';
 import styled from '@emotion/styled';
 import { Helmet } from 'react-helmet';
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
